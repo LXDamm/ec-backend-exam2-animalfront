23fe9566196f0706d23c6f66eab76143f7979af6
Lexx Damm <lexx.damm@gmail.com>
2021-05-17 01:29:09 +0200

    my first Vue component

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="my-component">
+      <p>Hello</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MyComponent",
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
